f775e86d7ad1f0870e501353de932e83afe6352a
Edisson Caicedo <edisson.caicedo1@gmail.com>
2022-11-14 21:51:01 -0500

    Version f

--- Grupo 21/backend/routes/routers.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const rutaClientes = require('./router_clientes');
-router.use("/clientes",rutaClientes);
-
-module.exports = router;
